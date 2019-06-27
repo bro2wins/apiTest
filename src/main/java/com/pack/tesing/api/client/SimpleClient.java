0c5cb37d50678061a7893d689d784dd73392b11e
2b <annaya15@mail.ru>
2019-06-27 18:52:36 +0400

    change SimpleClient

--- src/main/java/com/pack/tesing/api/client/SimpleClient.java
+++ src/main/java/com/pack/tesing/api/client/SimpleClient.java
@@ -1,39 +1,10 @@
 package com.pack.tesing.api.client;
 
-
-import com.pack.tesing.api.model.LoginData;
-import com.pack.tesing.api.model.Student;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public interface SimpleClient {
-
-    @POST
-    @Path("/test")
-    @Consumes("application/json")
-    @Produces("application/json")
-    Response loginUser(LoginData loginData);
-
-
-    @POST
-    @Path("/logout")
-    @Consumes(MediaType.APPLICATION_JSON)
-    Response logout(@QueryParam("id") String userId);
-
-
-    @GET
-    @Path("/test2")
-    @Consumes("application/json")
-    @Produces("application/json")
-    Response test();
-
-    @Path("/student")
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    Response saveStudentResponse(Student student);
-
 
     @Path("/users/{id}")
     @GET
